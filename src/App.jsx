74453da0f5f54b8eca6cece16360d2aa0e2ba442
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2025-07-29 17:27:58 +0300

    Add react-router-dom and update routing structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Contact from './Pages/Contact/Contact';
+import Layout from './Layout/Layout';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout/>,
+      children: [
+          {
+            path: "",
+            element: <Home />,
+          },
+          {
+            path: "about",
+            element: <About />,
+          },
+          {
+            path: "portfolio",
+            element: <Portfolio />,
+          },
+          {
+            path: "contact",
+            element: <Contact />,
+          },
+        ]
+      }
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
